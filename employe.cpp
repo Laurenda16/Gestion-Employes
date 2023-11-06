5bb5970bbc7cabd6e6f284a1e52a372530d28d80
Laurenda Edorh <laurenda98@gmail.com>
2023-11-06 18:19:39 +0100

    terminer la class employe

--- employe.cpp
+++ employe.cpp
@@ -1,1 +1,26 @@
+#include "employe.h"
+using std::cout;
+using std::cin;
+using std::string;
 
+ employe::employe(string nom, int indice):d_nom{nom},d_indice{indice}
+ {
+
+ }
+    int indice() const
+    {
+        return d_indice;
+    }
+    string nom() const
+    {
+        return d_nom;
+    }
+
+    int salaireMensuel() const
+    {
+        return 40*d_indice;
+    }
+    void affiche(std::ostream& ost) const
+    {
+       ost<< "Employer"<<d_nom<< "indice"<<d_indice<<"salaire"<< salaireMensuel();
+    }
--- employe.h
+++ employe.h
@@ -1,1 +1,26 @@
+#ifndef EMPLOYE_H
+#define EMPLOYE_H
+
+#include<string>
+#include<iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+
+class employe {
+public:
+    employe(string nom, int indice);
+    int indice() const;
+    string nom() const;
+    int salaireMensuel() const;
+    void affiche(std::ostream& ost) const;
+
+private:
+string d_nom;
+int d_indice;
+
+};
+
+#endif // EMPLOYE_H
 
